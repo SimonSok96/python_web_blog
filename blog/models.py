7c3c828c731ba0e846b0c6ed4304da77db821ff5
SimonSok96 <Sokolov.sam@gmail.com>
2023-11-02 20:11:32 +0200

    02.11.23

--- blog/models.py
+++ blog/models.py
@@ -3,6 +3,12 @@ from django.db import models
 from django.utils import timezone
 
 
+class PublishedManager(models.Manager):
+    def get_queryset(self):
+        return super().get_queryset().filter(status=Post.Status.PUBLISHED)
+
+
+
 class Post(models.Model):
 
     class Status(models.TextChoices):
@@ -13,12 +19,15 @@ class Post(models.Model):
     slug = models.SlugField(max_length=250)
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='blog_posts')
     body = models.TextField()
-    publish = models.DateTimeField(default=timezone)
+    publish = models.DateTimeField(default=timezone.now)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
     status = models.CharField(max_length=2,
                               choices=Status.choices,
                               default=Status.DRAFT)
+
+    objects = models.Manager()
+    published = PublishedManager()
 
     class Meta:
         ordering = ['-publish']
